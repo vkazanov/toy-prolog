4a5ec2c9ef0573f06f9d3f1b7ea162d1d512b781
Vladimir Kazanov <vkazanov@inbox.ru>
2016-03-15 23:13:27 +0000

    playing with input

--- main.c
+++ main.c
@@ -1,9 +1,25 @@
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char* argv[argc+1]) {
-    for (int i = 1; i < argc; i++) {
-        puts(argv[i]);
+void process_file(FILE* stream, char* delim) {
+    size_t bytes_to_read = 100;
+    char* buf = malloc (bytes_to_read+1);
+    while (getline(&buf, &bytes_to_read, stream) != -1) {
+        puts (buf);
     }
+    /* TODO:  */
+    if (delim && delim[0] != '\0') {
+        return;
+    }
+    return;
+}
+
+int main (int argc, char* argv[argc+1]) {
+    for (int i = 1; i < argc; i++) {
+        FILE* file = fopen (argv[i], "r");
+        process_file (file, "");
+    }
+    process_file(stdin, "? ");
     return EXIT_SUCCESS;
 }
