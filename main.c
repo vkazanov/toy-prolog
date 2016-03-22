be9d7338f3c5d1995efc34d5108e11fb17706601
Vladimir Kazanov <vkazanov@inbox.ru>
2016-03-22 23:02:04 +0000

    basic input string preparation

--- main.c
+++ main.c
@@ -4,10 +4,12 @@
 #include <stdbool.h>
 #include <errno.h>
 #include <string.h>
+#include <regex.h>
 
 void fail(const char* error);
 void procfile(FILE* stream, char* delim);
 void trim(char* str);
+void stripWhitespace(char* buf);
 
 int main (int argc, char* argv[argc+1]) {
     for (int i = 1; i < argc; i++) {
@@ -44,7 +46,36 @@ void procfile(FILE* stream, char* prompt) {
             free(buf);
             break;
         }
-        /* TODO:  */
+
+        /* Clip comments */
+        regex_t regex;
+        if (regcomp(&regex, "#.*", 0) != 0) {
+            regfree(&regex);
+            fail("Failed to compile a regular expression");
+        }
+        regmatch_t regmatch[1];
+        if (regexec(&regex, buf, 1, regmatch, 0) == 0) {
+            buf[regmatch[0].rm_so] = '\0';
+        }
+
+        /* Strip whitespaces */
+        stripWhitespace(buf);
+
+        /* nothing left, just continue to the next line */
+        if (strcmp(buf, "") == 0) {
+            free(buf);
+            continue;
+        }
+
+        /* TODO: punc */
+        /* TODO: trace/quit */
+        /* TODO: rules dump */
+        /* TODO: rule add */
+        /* TODO: search */
+
+        puts("results:");
+        puts(buf);
+
         free(buf);
     }
 }
@@ -54,4 +85,17 @@ void trim(char* str) {
     if (pos) {
         *pos = '\0';
     }
+}
+
+void stripWhitespace(char* buf) {
+    char* i = buf;
+    char* j = buf;
+    while (*j != '\0') {
+        *i = *j;
+        j++;
+        if (*i != ' ') {
+            i++;
+        }
+    }
+    *i = 0;
 }
