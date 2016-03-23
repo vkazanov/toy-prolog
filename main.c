fab4db71bcdc50643d4b13805c436cf4754ec989
Vladimir Kazanov <vkazanov@inbox.ru>
2016-03-23 23:00:38 +0000

    Clean up the code a bit

--- main.c
+++ main.c
@@ -7,9 +7,10 @@
 #include <regex.h>
 
 void fail(const char* error);
-void procfile(FILE* stream, char* delim);
+void proc_file(FILE* stream, char* delim);
 void trim(char* str);
-void stripWhitespace(char* buf);
+void remove_comments(char* buf);
+void strip_whitespace(char* buf);
 
 int main (int argc, char* argv[argc+1]) {
     for (int i = 1; i < argc; i++) {
@@ -21,10 +22,10 @@ int main (int argc, char* argv[argc+1]) {
         if (!file) {
             fail("Failed to open a file");
         }
-        procfile(file, "");
+        proc_file(file, "");
         fclose(file);
     }
-    procfile(stdin, "? ");
+    proc_file(stdin, "? ");
     return EXIT_SUCCESS;
 }
 
@@ -33,7 +34,7 @@ void fail(const char* error) {
     exit(EXIT_FAILURE);
 }
 
-void procfile(FILE* stream, char* prompt) {
+void proc_file(FILE* stream, char* prompt) {
     while (true) {
         if (strlen(prompt) != 0) {
             printf("%s", prompt);
@@ -41,40 +42,41 @@ void procfile(FILE* stream, char* prompt) {
         size_t bytes_to_read = 256;
         char* buf = malloc(bytes_to_read+1);
         getline(&buf, &bytes_to_read, stream);
+
         trim(buf);
+
+        /* Empty line? stop */
         if (strcmp(buf, "") == 0) {
             free(buf);
             break;
         }
 
-        /* Clip comments */
-        regex_t regex;
-        if (regcomp(&regex, "#.*", 0) != 0) {
-            regfree(&regex);
-            fail("Failed to compile a regular expression");
-        }
-        regmatch_t regmatch[1];
-        if (regexec(&regex, buf, 1, regmatch, 0) == 0) {
-            buf[regmatch[0].rm_so] = '\0';
-        }
+        remove_comments(buf);
+        strip_whitespace(buf);
 
-        /* Strip whitespaces */
-        stripWhitespace(buf);
-
-        /* nothing left, just continue to the next line */
+        /* Nothing left, just continue to the next line */
         if (strcmp(buf, "") == 0) {
             free(buf);
             continue;
         }
+
+        /* See what the command is */
+        size_t buflen = strlen(buf);
+        char punc = buf[buflen-1];
+        if (punc == '.' || punc == '?') {
+            buf[buflen-1] = '\0';
+        } else {
+            punc = '.';
+        }
+
+        printf("buf = '%s'", buf);
+        printf("punc = '%c'", punc);
 
         /* TODO: punc */
         /* TODO: trace/quit */
         /* TODO: rules dump */
         /* TODO: rule add */
         /* TODO: search */
-
-        puts("results:");
-        puts(buf);
 
         free(buf);
     }
@@ -87,7 +89,7 @@ void trim(char* str) {
     }
 }
 
-void stripWhitespace(char* buf) {
+void strip_whitespace(char* buf) {
     char* i = buf;
     char* j = buf;
     while (*j != '\0') {
@@ -98,4 +100,16 @@ void stripWhitespace(char* buf) {
         }
     }
     *i = 0;
+}
+
+void remove_comments(char* buf) {
+    regex_t regex;
+    if (regcomp(&regex, "#.*", 0) != 0) {
+        regfree(&regex);
+        fail("Failed to compile a regular expression");
+    }
+    regmatch_t regmatch[1];
+    if (regexec(&regex, buf, 1, regmatch, 0) == 0) {
+        buf[regmatch[0].rm_so] = '\0';
+    }
 }
